fa031115f017321eafc0ace830b713250d568380
Phunvh <fuuuungvh@gmail.com>
2026-08-27 15:09:20 +0700

    remove output export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // <-- BỔ SUNG DÒNG NÀY
   reactStrictMode: true,
   images: {
     unoptimized: true,
