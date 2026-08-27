fc66a5831cd0a0bfeebcf524ee1a81ea4698c403
Phunvh <fuuuungvh@gmail.com>
2026-08-27 15:01:17 +0700

    add output export config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', // <-- BỔ SUNG DÒNG NÀY
   reactStrictMode: true,
   images: {
     unoptimized: true,
